3c6bc46166a14e8c5a10c933dcbf59237dc25000
Nirbhay Singh <kumarnirbhay1986@gmail.com>
2021-10-18 20:04:05 -0500

    Added bootstrap

---
+++ src/app/airline-list/airline-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-airline-list',
+  templateUrl: './airline-list.component.html',
+  styleUrls: ['./airline-list.component.scss']
+})
+export class AirlineListComponent {
+   
+ 
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
 import { environment } from 'src/environments/environment';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { AirlineInMemDataService } from './services/airline-in-mem-data.service';
@@ -7,17 +9,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { AirlineListComponent } from './airline-list/airline-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent,
+    AirlineListComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    HttpClientInMemoryWebApiModule.forRoot(AirlineInMemDataService)
+    HttpClientInMemoryWebApiModule.forRoot(AirlineInMemDataService),
+    RouterModule.forRoot([
+      { path: '', redirectTo: '/airlines', pathMatch: 'full' },
+      { path: 'airlines', component: AirlineListComponent },
+      //{ path: 'treasures', component: TreasuresComponent },
+      //{ path: 'treasure-detail/:id', component: TreasureDetailComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
